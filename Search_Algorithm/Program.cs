b5105f708078f3dcb06afb9f834be725d70e1964
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-10-28 16:01:46 +0700

    Membuat class program, lalu membuat variabel n dan i

--- Search_Algorithm/Program.cs
+++ Search_Algorithm/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+namespace SearchAlgoritm
+{
+    class Program
+    {
+        //arrat to be searched
+        int[] arr = new int[20];
+        //number of element in the array 
+        int n;
+        //get number of elements to store in the array
+        int i;
+
+    }
+}
